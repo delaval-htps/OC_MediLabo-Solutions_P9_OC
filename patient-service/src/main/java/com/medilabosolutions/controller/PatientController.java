1b446ee3160d227daef349aae78d1fcdc8b02178
delaval.htps <delaval.htps@gmail.com>
2023-08-14 17:39:33 +0200

    add controller & service for patient-service

---
+++ patient-service/src/main/java/com/medilabosolutions/controller/PatientController.java
@@ -0,0 +1,100 @@
+package com.medilabosolutions.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import com.medilabosolutions.model.Patient;
+import com.medilabosolutions.service.PatientService;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+    private final PatientService patientService;
+
+    /**
+     * endpoint to return all patients in db
+     * 
+     * @return ResponseEntity with reactive stream of all patients if existing
+     */
+    @GetMapping
+    public ResponseEntity<Flux<Patient>> getAllPatients() {
+        return ResponseEntity.ok(patientService.findAll());
+    }
+
+    /**
+     * endpoint to find a patient given its id
+     * 
+     * @param patientId id of patient to find
+     * @return a reactive stream of ResponseEntity<Patient>:
+     *         <ul>
+     *         <li>- with HttpStatus.ok and patient in body, if patient was found</li>
+     *         <li>- with HttpStatus.notFound with empty patient if was not found</li>
+     *         </ul>
+     */
+    @GetMapping("/{id}")
+    public Mono<ResponseEntity<Patient>> getPatientById(@PathVariable("id") Long patientId) {
+        return patientService.findById(patientId).map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    /**
+     * create a patient
+     * 
+     * @param patient in json format in requestBody
+     * @return a reactive stream of ResponseEntity with HttpStatus.created and body created patient
+     */
+    @PostMapping
+    public Mono<ResponseEntity<Patient>> createPatient(@RequestBody Patient patient) {
+        return patientService.createPatient(patient)
+                .map(p -> new ResponseEntity<Patient>(patient, HttpStatus.CREATED));
+    }
+
+    /**
+     * update field of a existing patient
+     * 
+     * @param patientId id of patient to update
+     * @param patient fields in json format in requestBody
+     * @return a reactive stream of ResponseEntity :
+     *         <ul>
+     *         <li>- with HttpStatus.ok and patient in body, if update operation is ok</li>
+     *         <li>- with HttpStatus.badRequest with empty patient if update operation fails</li>
+     *         </ul>
+     */
+    @PutMapping(value = "/{id}")
+    public Mono<ResponseEntity<Patient>> updatePatient(@PathVariable("id") Long patientId,
+            @RequestBody Patient patient) {
+        return patientService.updatePatient(patientId, patient).map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.badRequest().build());
+    }
+
+    /**
+     * delete a patient by given his id
+     * 
+     * @param patientId id of patient to delete
+     * @return a reactive stream of ResponseEntity :
+     *         <ul>
+     *         <li>- with HttpStatus.ok and deleted patient in body, if delete operation is ok</li>
+     *         <li>- with HttpStatus.notfound with empty patient if delete operation fails because
+     *         of not found patient</li>
+     *         </ul>
+     */
+    @DeleteMapping("/{id}")
+    public Mono<ResponseEntity<Patient>> deletePatient(@PathVariable("id") Long patientId) {
+        return patientService.deleteById(patientId).map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+}
