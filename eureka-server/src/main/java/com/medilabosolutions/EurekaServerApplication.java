5c9ecd9ddd4b80c5893305b155cc91c98c2716ed
delaval.htps <delaval.htps@gmail.com>
2023-08-29 12:06:58 +0200

    Add eureka-server
    - resolve conflict in maven cf dependencyManagement
    - add annotation @enableEurekaServer in EurekaServerApplication
    - update application.properties to initialise eureka-server

---
+++ eureka-server/src/main/java/com/medilabosolutions/EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.medilabosolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
