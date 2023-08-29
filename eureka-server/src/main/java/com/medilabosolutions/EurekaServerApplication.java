fdd9ec380f6e45cd47aa4dd2d7c9a8545643cbc0
delaval.htps <delaval.htps@gmail.com>
2023-08-29 12:34:08 +0200

    Update import for eureka-server to use annotation

--- eureka-server/src/main/java/com/medilabosolutions/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/medilabosolutions/EurekaServerApplication.java
@@ -2,6 +2,7 @@ package com.medilabosolutions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
