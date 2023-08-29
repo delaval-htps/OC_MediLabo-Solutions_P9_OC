7b4a97a7dd13f625464ace101945d62b576653b3
delaval.htps <delaval.htps@gmail.com>
2023-08-29 10:15:51 +0200

    add routes config for patient-service

---
+++ gateway-service/src/main/java/com/medilabosolutions/configuration/SpringCloudConfig.java
@@ -0,0 +1,17 @@
+package com.medilabosolutions.configuration;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringCloudConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("patientService", r -> r.path("/patients/**").uri("http://localhost:8081"))
+                .build();
+    }
+}
