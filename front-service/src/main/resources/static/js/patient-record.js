4f6f2f631953c112079170f0f658f2a39913ce04
delaval-htps <delaval.htps@gmail.com>
2023-10-01 16:04:35 +0200

    update ux for front-service
    - add tooltips on btn
    - add btn to return on index page
    - add clickable icon to update fields
    - update font & size of fieldset

--- front-service/src/main/resources/static/js/patient-record.js
+++ front-service/src/main/resources/static/js/patient-record.js
@@ -1,18 +1,28 @@
-// let updateQueryString = location.search.substring(1)
-// console.log(updateQueryString)
-// let update = updateQueryString.split('=')[1]
 const urlParams = new URLSearchParams(window.location.search)
 const update = urlParams.get('update')
 console.log(update)
 
-if (update == 'false') {
-    togglePersonalInformationForm()
-}else{
-   
+
+if (update == 'true' || fieldsOnError == {}) {
+    document.querySelector('.bi-pencil-square').style.display = 'none'
+} else {
+    disablePersonalInformation()
 }
 
-function togglePersonalInformationForm() {
-    document.getElementById('submit-personal-information').style.display = "none"
-    document.querySelector('#form-update-patient fieldset').setAttribute("disabled", "disabled")
-    document.getElementById('genre').classList.replace('form-select','form-control')
+function disablePersonalInformation() {
+    document.querySelector('#div-submit-btn').style.display = "none"
+    document.querySelector('#form-update-patient fieldset').setAttribute("disabled", "true")
+    document.getElementById('genre').classList.replace('form-select', 'form-control')
+    document.getElementById('dateOfBirth').disabled = true
+    document.querySelector('.bi-pencil-square').style.display = 'block'
+    document.querySelector('.bi-x-square').style.display = 'none'
+}
+
+function enablePersonalInformation() {
+    document.querySelector('#div-submit-btn').style.display = 'flex'
+    document.querySelector('#form-update-patient fieldset').removeAttribute("disabled")
+    document.getElementById('genre').classList.replace('form-control', 'form-select')
+    document.getElementById('dateOfBirth').disabled = false
+    document.querySelector('.bi-pencil-square').style.display = 'none'
+    document.querySelector('.bi-x-square').style.display = 'block'
 }
