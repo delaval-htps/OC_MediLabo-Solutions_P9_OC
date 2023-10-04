b2a9071eb62d2df4d59beda2771ad2058e15b8fa
delaval-htps <delaval.htps@gmail.com>
2023-10-04 19:04:37 +0200

    add security authorization for 'ROLE'

---
+++ front-service/src/main/java/com/medilabosolutions/configuration/FrontSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.medilabosolutions.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+@Configuration
+public class FrontSecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(exchanges -> exchanges
+                .pathMatchers("/**").hasAuthority("USER")
+                .anyExchange().authenticated())
+                .csrf(CsrfSpec::disable);
+        return http.build();
+    }
+
+
+}
