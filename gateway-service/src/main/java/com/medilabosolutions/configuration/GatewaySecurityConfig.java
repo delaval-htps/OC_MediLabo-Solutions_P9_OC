1614dc22d2c64b2b821f6d14c269f82c836e21fa
delaval-htps <delaval.htps@gmail.com>
2023-10-07 15:12:07 +0200

    update filterSecurity to diasble httpbasic

--- gateway-service/src/main/java/com/medilabosolutions/configuration/GatewaySecurityConfig.java
+++ gateway-service/src/main/java/com/medilabosolutions/configuration/GatewaySecurityConfig.java
@@ -3,12 +3,10 @@ package com.medilabosolutions.configuration;
 import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
-import org.springframework.security.authorization.ReactiveAuthorizationManager;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
+import org.springframework.security.config.web.server.ServerHttpSecurity.HttpBasicSpec;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -24,17 +22,18 @@ public class GatewaySecurityConfig {
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        http.authorizeExchange(exchanges -> exchanges
-                .pathMatchers("/login").permitAll()
-                .pathMatchers("/front/**").permitAll()
-                .pathMatchers("/api/v1/patients/**").permitAll()
-                .anyExchange().authenticated())
-
+        http
                 .formLogin(login -> login
 
                         .authenticationSuccessHandler(new RedirectServerAuthenticationSuccessHandler("/front")))
 
-                .httpBasic(withDefaults())
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/login").permitAll()
+                        .pathMatchers("/front/**").permitAll()
+                        .pathMatchers("/api/v1/patients/**").permitAll()
+                        .anyExchange().authenticated())
+
+                .logout(withDefaults()).httpBasic(HttpBasicSpec::disable)
                 .csrf(CsrfSpec::disable);
         return http.build();
     }
