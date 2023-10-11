f12a99fa44e431694b0ab22496d7a610dbc3854b
delaval-htps <delaval.htps@gmail.com>
2023-10-11 21:04:47 +0200

    extract configuration of session in config class

--- front-service/src/main/java/com/medilabosolutions/configuration/FrontSecurityConfig.java
+++ front-service/src/main/java/com/medilabosolutions/configuration/FrontSecurityConfig.java
@@ -1,24 +1,20 @@
 package com.medilabosolutions.configuration;
 
-import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.session.ReactiveMapSessionRepository;
-import org.springframework.session.ReactiveSessionRepository;
-import org.springframework.session.config.annotation.web.server.EnableSpringWebSession;
 
 
 @Configuration
 @EnableWebFluxSecurity
-@EnableSpringWebSession
 public class FrontSecurityConfig {
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        
         http.authorizeExchange(exchanges -> exchanges
                 .pathMatchers( "/public/**", "/favicon.ico").permitAll()
                 // .pathMatchers("/**").permitAll())
@@ -29,9 +25,5 @@ public class FrontSecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public ReactiveSessionRepository reactiveSessionRepository() {
-        return new ReactiveMapSessionRepository(new ConcurrentHashMap<>());
-    }
 
 }
--- gateway-service/src/main/java/com/medilabosolutions/configuration/GatewaySecurityConfig.java
+++ gateway-service/src/main/java/com/medilabosolutions/configuration/GatewaySecurityConfig.java
@@ -1,6 +1,5 @@
 package com.medilabosolutions.configuration;
 
-import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -14,8 +13,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.session.ReactiveMapSessionRepository;
-import org.springframework.session.ReactiveSessionRepository;
 import org.springframework.session.config.annotation.web.server.EnableSpringWebSession;
 
 @Configuration
@@ -26,14 +23,12 @@ public class GatewaySecurityConfig {
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        http
 
-
-                .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/front/**").authenticated()
-                        .pathMatchers("/login","/").permitAll()
-                        .pathMatchers("/public/**","/favicon.ico","/patient-record/**","/patient/**").permitAll()
-                        .pathMatchers("/api/v1/patients/**").permitAll())
+        http.authorizeExchange(exchanges -> exchanges
+                .pathMatchers("/front/**").authenticated()
+                .pathMatchers("/login", "/").permitAll()
+                .pathMatchers("/public/**", "/favicon.ico", "/patient-record/**", "/patient/**").permitAll()
+                .pathMatchers("/api/v1/patients/**").permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(HttpBasicSpec::disable)
                 .csrf(CsrfSpec::disable);
@@ -55,9 +50,5 @@ public class GatewaySecurityConfig {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    @Bean
-    public ReactiveSessionRepository reactiveSessionRepository() {
-        return new ReactiveMapSessionRepository(new ConcurrentHashMap<>());
-    }
 
 }
