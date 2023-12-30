a353aefd99d94916e2e25de7aad4d4e9f2c0f450
delaval-htps <delaval.htps@gmail.com>
2023-12-30 19:31:46 +0100

    add dockerfile , update pom.xml &  add profile test for risk-service

--- risk-service/src/test/java/com/medilabosolutions/controller/RiskControllerTest.java
+++ risk-service/src/test/java/com/medilabosolutions/controller/RiskControllerTest.java
@@ -3,9 +3,11 @@ package com.medilabosolutions.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles(value = "test")
  class RiskControllerTest {
 
     // @Autowired
--- risk-service/src/test/java/com/medilabosolutions/service/DiabetesRiskServiceTest.java
+++ risk-service/src/test/java/com/medilabosolutions/service/DiabetesRiskServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import com.medilabosolutions.dto.AssessmentDto;
@@ -15,6 +16,7 @@ import com.medilabosolutions.model.ExpectedRisk;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {DiabetesRiskService.class})
+@ActiveProfiles("test")
 class DiabetesRiskServiceTest {
 
     @Autowired
